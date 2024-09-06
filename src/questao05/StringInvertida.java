610064774ecd348c4fbdb92ca11b57f47ff48cae
alanmarques144 <alanshalom12@gmail.com>
2024-09-06 10:19:36 -0300

    questões finalizadas!

--- src/questao05/StringInvertida.java
+++ src/questao05/StringInvertida.java
@@ -1,5 +1,13 @@
 package questao05;
 
+/*
+ 5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+ */
+
 import java.util.Scanner;
 
 public class StringInvertida {
